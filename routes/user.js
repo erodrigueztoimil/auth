7056038edf03497eba4deae837890262092dfb2b
Ernesto Rodriguez <erodrigueztoimil@gmail.com>
2020-09-20 22:22:34 -0400

    latest updates to user api routes

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const User = require("../models/user.model");
 const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const saltRounds = 20;
 
 const pwdHashHandler = (pwd) => {
   return new Promise((response) => {
